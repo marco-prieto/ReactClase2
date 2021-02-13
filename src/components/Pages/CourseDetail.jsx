17d884cd617c6cad5a18cdc28451c22f021872ea
Marco <marcoztk@gmail.com>
2021-02-13 16:42:21 -0500

    hookear a un crrano

--- src/components/Pages/CourseDetail.jsx
+++ src/components/Pages/CourseDetail.jsx
@@ -1,6 +1,8 @@
-import React from "react"
+import React,{useState,useEffect} from "react"
+import axios from 'axios'
 
-const courses = [
+
+/* const courses = [
   {
       "id": 1,
     "title": "React desde cero",
@@ -29,11 +31,14 @@ const courses = [
     "price": 44,
     "profesor": "Alfred Goy"
   }  
-]
+] */
+
 //ese match creo q sirve pa agarrar el id q mandamos x otro lado puede ser cualquier cosa luego de los :id :aea :etc xD
 const CourseDetail = ({match}) => {
     //asumo q filter crea otro arreglo pero al final sera solo de 1 asi q [0] agarraria el primero y unico asumiento q es una lista con un objeto .. solo asumo pero seguro q esxD
-    const CursoActual = courses.filter(c => c.id === parseInt(match.params.id))[0]
+    
+    //sin api haciendo filter
+    /* const CursoActual = courses.filter(c => c.id === parseInt(match.params.id))[0] */
 
     //mi forma esta correcto
     /* return (
@@ -52,15 +57,44 @@ const CourseDetail = ({match}) => {
     ) */
     //forma del profe como pa ver variantes  y entender un poco mas la sintaxis
 
+    //el 2do estado y el 2do es un colback (los nombres con fines didacticos puedo darle cualquier nombre a la variable)
+    const [course, setCourse] = useState({})
+    const [comment,setComment] = useState("Sin comentarios")
+    //cada vez q el componente se renderize useEffect funca
+    useEffect(() => {
+        
+        axios.get(`http://my-json-server.typicode.com/marco-prieto/json-db/cursos/${match.params.id}`).then(resp => setCourse(resp.data))
+    },[])
+    //ese 2do parametro es para q no  c produzca infinitamente el useEffect
+
+    const changeTitle = (text) => {
+        setCourse(
+            {
+                ...course,
+                title : text
+            }
+        )
+    }
+    const myComment = e => {
+        setComment(e.target.value)
+    }
     return(
         <div className="ed-grid m-grid-3">
             {
-                CursoActual ? (
-                    <>  
-                        <h1 className="m-cols-3">{CursoActual.title}</h1>
-                        <img className="m-cols-1" src={CursoActual.imagen}/>
-                        <p className="m-cols-2">Profesor: {CursoActual.profesor}</p>
-                    </>
+                course ? (
+                    <div className="ed-grid">  
+                        <div className="l-block">
+                            <h1 className="m-cols-3">{course.title}</h1>
+                            <img className="m-cols-1" src={course.imagen}/>
+                            <p className="m-cols-2">Profesor: {course.profesor}</p>
+                            <button className="button" onClick={changeTitle.bind(this,"Titulo Nuevo")}>Cambiar titulo</button>
+                        </div>
+                        <div className="ed-grid">
+                            <h2>Escribe tu comentario</h2>
+                            <input type="text" placeholder="Escribe..." onChange={myComment.bind(this)}/>
+                            <p>{comment}</p>
+                        </div>
+                    </div>
                 ) :
                 <h1>El curso no existe</h1>
             }
